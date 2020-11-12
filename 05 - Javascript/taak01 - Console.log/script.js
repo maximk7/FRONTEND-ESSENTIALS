24b0a2c5630356d81cda34fc1d73eaa9079f261f
Maxim Krymchansky <maxim.krymchansky@student.rocva.nl>
2020-11-12 21:29:02 +0100

    change

---
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -0,0 +1,16 @@
+console.log("mijn naam is maxim");
+let mijnvoornaam = "maxim"
+let mijnachternaam = "krymchansky"
+let leeftijd = "19"
+let ikBenStemGerechtig = "ja"
+let ikMagLegaalAlcoholKopen = "ja"
+let ikMagBrommerRijden = "nee"
+let SoftwareOntwikkeling = "opzich"
+console.log("mijnvoornaam")
+console.log("mijnachternaam")
+console.log("leeftijd")
+console.log("ikbenstemgerechtig")
+console.log("ikmaglegaalalcoholkopen")
+console.log("ikmagbrommerrijden")
+console.log("softwareontwikkeling")
+
